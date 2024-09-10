c8c195200aaab26e5bac36a18e2dc00de2d4fa99
oussama <ossamahattan@gmail.com>
2024-09-10 18:39:02 +0100

    chore: Add new shop category CSS file

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -5,6 +5,10 @@ import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import Shop from './Pages/Shop';
 import Product from './Pages/Product';
 import ShopCategory from './Pages/ShopCategory';
+import Footer from './Components/Footer/Footer';
+import men_banner from './Assets/Frontend_Assets/banner_mens.png'
+import women_banner from './Assets/Frontend_Assets/banner_women.png'
+import kid_banner from './Assets/Frontend_Assets/banner_kids.png'
 
 
 function App() {
@@ -14,14 +18,15 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path='/' element={<Shop/>}/>
-        <Route path='/mens' element={<ShopCategory category="men"/>}/>
-        <Route path='/womens' element={<ShopCategory category="women"/>}/>
-        <Route path='/kids' element={<ShopCategory category="kid"/>}/>
+        <Route path='/mens' element={<ShopCategory banner={men_banner} category="men"/>}/>
+        <Route path='/womens' element={<ShopCategory banner={women_banner} category="women"/>}/>
+        <Route path='/kids' element={<ShopCategory banner={kid_banner} category="kid"/>}/>
         <Route path="/product" element={<Product/>}>
           <Route path=':productId' element={<Product/>}/>
         </Route>
         <Route path='/cart' element={<Shop/>}/>
       </Routes>
+      <Footer/>
       </BrowserRouter>
     </div>
   );
--- front_end/src/Components/NewCollections/NewCollections.jsx
+++ front_end/src/Components/NewCollections/NewCollections.jsx
@@ -8,7 +8,7 @@ const NewCollections = () => {
       <h1>NEW COLLECTION</h1>
       <hr />
       <div className="collections">
-        {new_collection.map((item)=>{
+        {new_collection.map((item,i)=>{
           return <Item key={item.id} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
         })}
       </div>
